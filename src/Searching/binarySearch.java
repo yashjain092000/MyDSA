179c9fce91c75443a03f5d6d32b9c397a0093a4c
yashjain092000 <yashjain092000@gmail.com>
2021-05-04 01:11:00 +0530

    add binary search recursive approach.

--- src/Searching/binarySearch.java
+++ src/Searching/binarySearch.java
@@ -16,6 +16,20 @@ public class binarySearch {
         }
         return -1;
     }
+    public int bSearchRecursive(int[] a,int key,int left,int right){
+        if(left>right)
+            return -1;
+        else{
+            int mid= (int) Math.floor((left+right)/2);
+            if(key==a[mid])
+                return mid;
+            if(key<a[mid])
+                return bSearchRecursive(a,key,left,mid-1);
+            if(key>a[mid])
+                return bSearchRecursive(a,key,mid+1,right);
+        }
+        return -1;
+    }
 
 
 
@@ -23,5 +37,6 @@ public class binarySearch {
         int[] a={1,2,3,4,5};
         binarySearch obj=new binarySearch();
         System.out.println(obj.bSearchIterative(a,4));
+        System.out.println(obj.bSearchRecursive(a,4,0,4));
     }
 }
