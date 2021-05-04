7e760ee90070f2c12bbcc1e3cf6aa792f1b84be0
yashjain092000 <yashjain092000@gmail.com>
2021-05-04 23:19:40 +0530

    add shell sort(my version)

--- src/Sorting/shellSort.java
+++ src/Sorting/shellSort.java
@@ -1,6 +1,10 @@
 package Sorting;
 
-//
+//O(nlogn)
+//Unstable
+//1. Initialize a gap variable to n/2.
+//2. Scan the array with a diff of gap+i to left and right of i, swap accordingly.
+//3. Repeat for gap/=2 until gap>0.
 public class shellSort {
     public void sort(int[] a){
         int gap=a.length/2;
