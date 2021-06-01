83ce5775db4fab3cea5143b2844bc5035a5e6627
yashjain092000 <yashjain092000@gmail.com>
2021-06-02 01:02:54 +0530

    remove my height method.

--- src/Trees/BinarySearchTrees/BinarySearchTree.java
+++ src/Trees/BinarySearchTrees/BinarySearchTree.java
@@ -5,87 +5,89 @@ import java.util.Queue;
 
 public class BinarySearchTree {
 
-    public class Node{
+    public class Node {
         int val;
         Node left;
         Node right;
-        Node(int value, Node l, Node r){
-            val=value;
-            left=l;
-            right=r;
+
+        Node(int value, Node l, Node r) {
+            val = value;
+            left = l;
+            right = r;
         }
     }
 
     Node root;
-    BinarySearchTree(){
-        root=null;
+
+    BinarySearchTree() {
+        root = null;
     }
 //Insertions
 
     //Iterative Insertion
-    public void insert(Node troot,int e){
-        Node temp=null;// for storing parent of node to be inserted
-        while(troot!=null){
-            temp=troot;
-            if(e==troot.val)
+    public void insert(Node troot, int e) {
+        Node temp = null;// for storing parent of node to be inserted
+        while (troot != null) {
+            temp = troot;
+            if (e == troot.val)
                 return;
-            else if(e<troot.val)
-                troot=troot.left;
-            else if(e>troot.val)
-                troot=troot.right;
+            else if (e < troot.val)
+                troot = troot.left;
+            else if (e > troot.val)
+                troot = troot.right;
         }
-        Node n=new Node(e,null,null);
-        if(root!=null){
-            if(e<temp.val)
-                temp.left=n;
+        Node n = new Node(e, null, null);
+        if (root != null) {
+            if (e < temp.val)
+                temp.left = n;
             else
-                temp.right=n;
-        }
-        else
-            root=n;
+                temp.right = n;
+        } else
+            root = n;
 
     }
 
     //Recursive Insertion
-    Node rInsert(Node troot, int e){
-        if(troot!=null){
-            if(e< troot.val)
-                troot.left=rInsert(troot.left,e);
-            else if(e>troot.val)
-                troot.right=rInsert(troot.right,e);
-        }
-        else{
-            Node n=new Node(e,null,null);
-            troot=n;
+    Node rInsert(Node troot, int e) {
+        if (troot != null) {
+            if (e < troot.val)
+                troot.left = rInsert(troot.left, e);
+            else if (e > troot.val)
+                troot.right = rInsert(troot.right, e);
+        } else {
+            Node n = new Node(e, null, null);
+            troot = n;
 
         }
         return troot;
     }
-//Traversals
-    void inorder(Node troot){
-        if(troot!=null){
+
+    //Traversals
+    void inorder(Node troot) {
+        if (troot != null) {
             inorder(troot.left);
-            System.out.print(troot.val+" ");
+            System.out.print(troot.val + " ");
             inorder(troot.right);
         }
     }
 
-    void preorder(Node troot){
-        if(troot!=null){
-            System.out.print(troot.val+" ");
+    void preorder(Node troot) {
+        if (troot != null) {
+            System.out.print(troot.val + " ");
             preorder(troot.left);
             preorder(troot.right);
         }
     }
 
-    void postorder(Node troot){
-        if(troot!=null){
+    void postorder(Node troot) {
+        if (troot != null) {
             postorder(troot.left);
             postorder(troot.right);
-            System.out.print(troot.val+" ");
+            System.out.print(troot.val + " ");
         }
 
     }
+
     void level() {
         Queue<Node> q = new LinkedList<>();
         Node t = root;
@@ -103,116 +105,100 @@ public class BinarySearchTree {
             }
         }
     }
-//Searching
-        //Iterative Searching
-        boolean search(int key){
-            Node troot=root;
-            while(troot!=null){
-                if(key==troot.val)
-                    return true;
-                else if(key<troot.val)
-                    troot=troot.left;
-                else if(key>troot.val)
-                    troot=troot.right;
-            }
-            return false;
-        }
 
-        //Recursive Searching
-        boolean rSearch(Node troot,int key){
-            if(troot==null)
-                return false;
-            else if(troot.val==key)
+    //Searching
+    //Iterative Searching
+    boolean search(int key) {
+        Node troot = root;
+        while (troot != null) {
+            if (key == troot.val)
                 return true;
-            else if(key<troot.val)
-                return rSearch(troot.left,key);
-            else
-                return rSearch(troot.right,key);
+            else if (key < troot.val)
+                troot = troot.left;
+            else if (key > troot.val)
+                troot = troot.right;
         }
+        return false;
+    }
 
-        //Deletion
-    boolean delete(int e){
-        Node p=root;
-        Node pp=null;
-        while(p!=null && p.val!=e){
-            pp=p;
-            if(e<p.val)
-                p=p.left;
+    //Recursive Searching
+    boolean rSearch(Node troot, int key) {
+        if (troot == null)
+            return false;
+        else if (troot.val == key)
+            return true;
+        else if (key < troot.val)
+            return rSearch(troot.left, key);
+        else
+            return rSearch(troot.right, key);
+    }
+
+    //Deletion
+    boolean delete(int e) {
+        Node p = root;
+        Node pp = null;
+        while (p != null && p.val != e) {
+            pp = p;
+            if (e < p.val)
+                p = p.left;
             else
-                p=p.right;
+                p = p.right;
         }
-        if(p==null)
+        if (p == null)
             return false;
         if (p.left != null && p.right != null) {
 
-            Node s=p.left;
-            Node ps=p;
-            while(s.right!=null){
-                ps=s;
-                s=s.right;
+            Node s = p.left;
+            Node ps = p;
+            while (s.right != null) {
+                ps = s;
+                s = s.right;
             }
-            p.val=s.val;
-            p=s;
-            pp=ps;
+            p.val = s.val;
+            p = s;
+            pp = ps;
         }
-        Node c=null;
-        if(p.left!=null)
-            c=p.left;
+        Node c = null;
+        if (p.left != null)
+            c = p.left;
         else
-            c=p.right;
-        if(p==root)
-            root=null;
+            c = p.right;
+        if (p == root)
+            root = null;
+        else if (p == pp.left)
+            pp.left = c;
         else
-            if(p==pp.left)
-                pp.left=c;
-            else
-                pp.right=c;
+            pp.right = c;
         return true;
     }
 
     //Count no. of Nodes
-    int count(Node troot){
-        if(troot!=null){
-            int x=count(troot.left);
-            int y=count(troot.right);
-            return x+y+1;
+    int count(Node troot) {
+        if (troot != null) {
+            int x = count(troot.left);
+            int y = count(troot.right);
+            return x + y + 1;
         }
         return 0;
     }
+
     //Height of tree
-    int height(Node troot){
-        if(troot!=null){
-            int x=height(troot.left);
-            int y=height(troot.right);
-           if(x>y)
-               return x+1;
-           else
-               return y+1;
+    int height(Node troot) {
+        if (troot != null) {
+            int x = height(troot.left);
+            int y = height(troot.right);
+            if (x > y)
+                return x + 1;
+            else
+                return y + 1;
         }
         return 0;
     }
 
-    int myHeight(Node troot){
-        if(troot!=null) {
-            int x = 0, y = 0;
-            Node r = troot;
-            while (troot != null) {
-                x++;
-                troot = troot.left;
-            }
-            while (r != null) {
-                y++;
-                r = r.right;
-            }
-            return (int) Math.max(x, y) + 1;
-        }
-
-        return 0;
-    }
 
 
     public static void main(String[] args) {
-        BinarySearchTree t=new BinarySearchTree();
+        BinarySearchTree t = new BinarySearchTree();
         t.insert(t.root, 50);
         t.insert(t.root, 30);
         t.insert(t.root, 80);
@@ -222,8 +208,8 @@ public class BinarySearchTree {
         t.insert(t.root, 90);
         t.inorder(t.root);
         System.out.println("recursive insert");
-        BinarySearchTree b=new BinarySearchTree();
-        b.root=b.rInsert(b.root, 10);
+        BinarySearchTree b = new BinarySearchTree();
+        b.root = b.rInsert(b.root, 10);
         b.rInsert(b.root, 40);
         b.rInsert(b.root, 30);
         b.rInsert(b.root, 60);
@@ -238,7 +224,7 @@ public class BinarySearchTree {
         System.out.println("");
         t.postorder(t.root);
         System.out.println(t.search(0));
-        System.out.println(t.rSearch(t.root,10));
+        System.out.println(t.rSearch(t.root, 10));
         System.out.println("");
         b.level();
         System.out.println("");
@@ -247,8 +233,8 @@ public class BinarySearchTree {
         b.level();
         System.out.println("");
         System.out.println(b.count(b.root));
-        System.out.println(t.height(t.root)-1);
-        System.out.println(b.myHeight(b.root)-1);
+        System.out.println(t.height(t.root) - 1);
+        System.out.println(b.myHeight(b.root) - 1);
 
 
     }
