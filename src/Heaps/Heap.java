e8af455098b3fb09d30c5dbfbc3e5b26408cabb8
yashjain092000 <yashjain092000@gmail.com>
2021-06-07 18:40:36 +0530

    add insert method in Heap data structure updated

--- src/Heaps/Heap.java
+++ src/Heaps/Heap.java
@@ -6,9 +6,9 @@ public class Heap {
     private int mSize;
     private int cSize;
     private int[] data;
-    Heap(int mSize){
-        this.mSize=mSize;
-        cSize=1;
+    Heap(){
+        mSize=10;
+        cSize=0;
         data=new int[mSize];
         Arrays.fill(data,0,mSize-1,-1);
     }
@@ -28,11 +28,34 @@ public class Heap {
         }
         cSize++;
         int hi=cSize;
-        while(hi>1 && e>data[(int)Math.floor(hi/2)]){
-            data[hi]=data[(int)Math.floor(hi/2)];
-            hi=(int)Math.floor(hi/2);
+        while(hi>1 && e>data[hi/2]){
+            data[hi]=data[hi/2];
             hi=hi/2;
         }
         data[hi]=e;
+    }
+    public int max(){
+        if(isEmpty()){
+            System.out.println("Heap is Empty");
+            return -1;
+            }
+        return data[1];
+    }
+    public void display(){
+        for(int i=1;i<data.length;i++){
+            System.out.print(data[i]+" ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        Heap h=new Heap();
+        h.insert(25);
+        h.insert(14);
+        h.insert(2);
+        h.insert(20);
+        h.insert(10);
+        h.display();
+
     }
 }
